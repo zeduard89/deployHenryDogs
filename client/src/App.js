25dcd9ef68b697f6eda934708e7dab3bd10dd436
Guillermo <zeduard89@gmail.com>
2023-05-30 19:32:17 -0300

    befor update

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import {Route , Routes, useNavigate} from 'react-router-dom';
 import {Cards,CardsTemps,Detail,Error,FormLogin,FormRegister,FormDog,NavBar,SearchBar,BreedGruop} from './components/RoutesConection/RoutesConection'
 import { connect } from 'react-redux';
 import axios from 'axios';
-axios.defaults.baseURL = 'https://deployhenrydogs-production.up.railway.app'
+axios.defaults.baseURL = 'https://deployhenrydogs-production.up.railway.app/'
 
 function App({logStatus}) {
   
