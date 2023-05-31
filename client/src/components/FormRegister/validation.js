a36dd98bef28ef184ec90f9275c7eb027752b9a4
Guillermo <zeduard89@gmail.com>
2023-05-31 12:49:46 -0300

    responsive

--- client/src/components/FormRegister/validation.js
+++ client/src/components/FormRegister/validation.js
@@ -1,33 +1,36 @@
+const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+const minPasswordLength = 6;
+const maxPasswordLength = 10;
+
+/**
+@param {object} userData - Los datos del usuario a validar.
+@returns {object} - Un objeto que contiene errores de validación, si los hay.
+*/
+
+
 export const validation = (userData) => {
+  let errors = {};
 
-    const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-    
-    
-        let errors = {};
-        
-        if (!userData.name){
-            errors.name = 'Ingrese un nombre valido'
-          }else if(userData.name.length < 5){
-              errors.name = 'Ingrese un nombre mayor a 5 caracteres'
-          }
-    
-        if (!regexEmail.test(userData.email)){
-          errors.email = 'Ingrese un email valido'
-        }else if(userData.email.length > 35){
-            errors.email = 'Ingrese un email menor a 35 caracteres'
-        }
+  if (!userData.name) {
+    errors.name = 'Ingrese un nombre válido';
+  } else if (userData.name.length < 5) {
+    errors.name = 'Ingrese un nombre mayor a 5 caracteres';
+  }
 
-        if(userData.password.length < 6 || userData.password.length > 10){
-            errors.password = 'Ingrese un password entre 6 y 10 caracteres'
-        }else if(!userData.password.match(/\d/)){
-            errors.password = 'Ingrese al menos un numero';
-        }        
-        
-        
+  if (!emailRegex.test(userData.email)) {
+    errors.email = 'Ingrese un email válido';
+  } else if (userData.email.length > 35) {
+    errors.email = 'Ingrese un email menor a 35 caracteres';
+  }
 
-        return errors;
+  if (userData.password.length < minPasswordLength || userData.password.length > maxPasswordLength) {
+    errors.password = `Ingrese un password entre ${minPasswordLength} y ${maxPasswordLength} caracteres`;
+  } else if (!userData.password.match(/\d/)) {
+    errors.password = 'Ingrese al menos un número';
+  }
 
-    }
+  return errors;
+};
 
 export default validation;
 
